eb40494e1f16f78c141d4b66cf5ba037b17a16fc
MichaelClaytonNoble <makos.code@gmail.com>
2021-04-19 13:26:29 -0700

    add simple google analytics on play button

--- src/index.js
+++ src/index.js
@@ -2,4 +2,10 @@ import GetOutTheWay from './GetOutTheWay.js'; //handles all logic for the entire
 
 document.addEventListener("DOMContentLoaded", ()=>{
   const GOTW = new GetOutTheWay();  
-})
+
+  setGAEvents();
+});
+
+function setGAEvents(){
+  document.getElementById('play').onclick = handleGAEvent('play', 'play', 'Game');
+}
